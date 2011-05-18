c8ffc8fb5906d6d3545bd301932d47d9a456095d
nerbie69 <philipjingram@yahoo.com>
2011-05-18 08:21:37 -0400

    Adding rails 3 functionality, final piece

--- lib/active_exchange.rb
+++ lib/active_exchange.rb
@@ -1,1 +1,8 @@
- require 'active_exchange' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+require 'active_exchange' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+require 'rails'
+
+module ActiveExchange
+  class Engine < Rails::Engine
+    # engine_name :active_exchange
+  end
+end
